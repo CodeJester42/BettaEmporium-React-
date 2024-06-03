d9c6b7c7c7b37f17035092cdccc2f84df5276680
CodeJester42 <MRNEALFERNANDEZ@GMAIL.COM>
2024-06-03 15:43:59 -0500

    minor changes to footer style

--- main/frontend/src/Pages/ShopCategory.jsx
+++ main/frontend/src/Pages/ShopCategory.jsx
@@ -25,7 +25,7 @@ const ShopCategory = (props) => {
       <div className='shopcategory-products'>
         {all_product.map((item, i)=>{
           if  (props.category===item.category) {
-            return <Item key={i} name={item.name} image={item.image} price={item.price} />
+            return <Item key={i}image={item.image} name={item.name} price={item.price} />
           }
           else {
             return null;
